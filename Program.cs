836568a1b1d0dc7d00415952a0537022151619ad
sonth123 <sonth@tmhpp.com.vn>
2026-06-10 16:25:54 +0700

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿# region BaiTap1
+// Tính tổng các số trong 1 mảng
+int[] lstNumber = {20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20};
+int sum = 0;
+foreach (int number in lstNumber)
+{
+    sum += number;
+}
+Console.WriteLine("Tổng các số trong mảng là: " + sum);
+
+#endregion
